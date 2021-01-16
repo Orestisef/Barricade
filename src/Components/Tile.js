72542a5a16b1d24d77b2c06d870a1391bb0a1a38
MeleteAshtoreth <kourousimaria07@gmail.com>
2021-01-16 20:37:47 +0200

    Customize tiles based on their type

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -4,8 +4,25 @@ export default class Tile extends Component {
 
     render(){
         console.log(this.props.number)
+        let tileType
+        if (this.props.number == 0) {
+            tileType = "invisible"
+        }
+        else if (this.props.number == 1) {
+            tileType = "tile"
+        }
+        else if (this.props.number == 2) {
+            tileType = "tile barricade"
+        }
+        else if (this.props.number == 3) {
+            tileType = "tile goal"
+        }
+        else {
+            tileType = "tile players"
+        }
+
         return(
-            <div className={this.props.number != 0 ? "tile" : "invisible"} >
+            <div className={tileType}>
                  {this.props.number}
             </div>
         )
